2c2f8e0286bca16d42983a078dfa767a0974c5d0
mrptsai <mrptsai@icloud.com>
2017-02-27 18:54:17 +1100

    changed disk config

--- active-directory/CreateADBDC.ps1
+++ active-directory/CreateADBDC.ps1
@@ -1,7 +1,7 @@
-﻿configuration CreateADBDC 
-{ 
-   param 
-    ( 
+﻿configuration CreateADBDC
+{
+    param
+    (
         [Parameter(Mandatory)]
         [String]$DomainName,
 
@@ -10,85 +10,85 @@
 
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
-    ) 
-    
+    )
+
     Import-DscResource -ModuleName xActiveDirectory, xDisk, cDisk
-    
+
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-   
+
     Node localhost
     {
-		xWaitforDisk Disk2
+        xWaitforDisk Disk1
         {
-             DiskNumber = 2
-             RetryIntervalSec =$RetryIntervalSec
-             RetryCount = $RetryCount
+            DiskNumber = 1
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
         }
 
         cDiskNoRestart ADDataDisk
         {
-            DiskNumber = 2
-            DriveLetter = "F"
-			DependsOn = "[xWaitForDisk]Disk2"
+            DiskNumber = 1
+            DriveLetter = "D"
+            DependsOn = "[xWaitForDisk]Disk1"
         }
 
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
             Name = "AD-Domain-Services"
-			DependsOn = "[cDiskNoRestart]ADDataDisk"
-        } 
+            DependsOn = "[cDiskNoRestart]ADDataDisk"
+        }
 
-		WindowsFeature ADAdminCenter 
-        { 
-            Ensure = "Present" 
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
             Name = "RSAT-AD-AdminCenter"
-			DependsOn = "[WindowsFeature]ADDSInstall"
+            DependsOn = "[WindowsFeature]ADDSInstall"
         }
-		
-		WindowsFeature ADDSTools 
-        { 
-            Ensure = "Present" 
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
             Name = "RSAT-ADDS-Tools"
-			DependsOn = "[WindowsFeature]ADDSInstall"
-        }  
-
-        xWaitForADDomain DscForestWait 
-        { 
-            DomainName = $DomainName 
-            DomainUserCredential= $DomainCreds
-            RetryCount = $RetryCount 
-            RetryIntervalSec = $RetryIntervalSec
-			DependsOn = "[WindowsFeature]ADDSInstall"
-        } 
-
-        xADDomainController BDC 
-        { 
-            DomainName = $DomainName 
-            DomainAdministratorCredential = $DomainCreds 
-            SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "F:\NTDS"
-            LogPath = "F:\NTDS"
-            SysvolPath = "F:\SYSVOL"
-			DependsOn = "[xWaitForADDomain]DscForestWait"
+            DependsOn = "[WindowsFeature]ADDSInstall"
         }
-	
-		Script script1
-		{
-      	    SetScript =  { 
-			$dnsFwdRule = Get-DnsServerForwarder
-			if ($dnsFwdRule) { Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force }
-					Write-Verbose -Verbose "Removing DNS forwarding rule" 
+
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        xADDomainController BDC
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "D:\NTDS"
+            LogPath = "D:\NTDS"
+            SysvolPath = "D:\SYSVOL"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        Script script1
+        {
+            SetScript =  {
+                $dnsFwdRule = Get-DnsServerForwarder
+                if ($dnsFwdRule) { Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force }
+                Write-Verbose -Verbose "Removing DNS forwarding rule"
             }
             GetScript =  { @{} }
             TestScript = { $false}
-			DependsOn = "[xADDomainController]BDC"
+            DependsOn = "[xADDomainController]BDC"
         }
 
-        LocalConfigurationManager 
+        LocalConfigurationManager
         {
-       	    ConfigurationMode = 'ApplyOnly'
+            ConfigurationMode = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
-  }
-} 
+    }
+}
--- active-directory/CreateADPDC.ps1
+++ active-directory/CreateADPDC.ps1
@@ -1,7 +1,7 @@
-﻿configuration CreateADPDC 
-{ 
-   param 
-   ( 
+﻿configuration CreateADPDC
+{
+    param
+    (
         [Parameter(Mandatory)]
         [String]$DomainName,
 
@@ -10,8 +10,8 @@
 
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
-    ) 
-    
+    )
+
     Import-DscResource -ModuleName xActiveDirectory,xDisk, xNetworking, cDisk, PSDesiredStateConfiguration
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
@@ -19,86 +19,86 @@
 
     Node localhost
     {
-		Script script1
-		{
-      	    SetScript =  { 
-				Set-DnsServerDiagnostics -All $true
-                Write-Verbose -Verbose "Enabling DNS client diagnostics" 
+        Script script1
+        {
+            SetScript =  {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics"
             }
             GetScript =  { @{} }
             TestScript = { $false}
-			DependsOn = "[WindowsFeature]DNS"
-        }
-	
-		WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"		
+            DependsOn = "[WindowsFeature]DNS"
         }
 
-		WindowsFeature DnsTools
-		{
-			Ensure = "Present"
-            Name = "RSAT-DNS-Server"
-		}
-
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = $InterfaceAlias
-            AddressFamily  = 'IPv4'
-			DependsOn = "[WindowsFeature]DNS"
-        }
-
-        xWaitforDisk Disk2
+        WindowsFeature DNS
         {
-             DiskNumber = 2
+            Ensure = "Present"
+            Name = "DNS"
+        }
+
+        WindowsFeature DnsTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+        }
+
+        xDnsServerAddress DnsServerAddress
+        {
+            Address = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
+        xWaitforDisk Disk1
+        {
+             DiskNumber = 1
              RetryIntervalSec =$RetryIntervalSec
              RetryCount = $RetryCount
         }
 
         cDiskNoRestart ADDataDisk
         {
-            DiskNumber = 2
-            DriveLetter = "F"
+            DiskNumber = 1
+            DriveLetter = "D"
         }
 
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
             Name = "AD-Domain-Services"
-			DependsOn= "[cDiskNoRestart]ADDataDisk"
-        }  
-
-		WindowsFeature ADAdminCenter 
-        { 
-            Ensure = "Present" 
-            Name = "RSAT-AD-AdminCenter"
-			DependsOn = "[WindowsFeature]ADDSInstall"
+            DependsOn = "[cDiskNoRestart]ADDataDisk"
         }
-		
-		WindowsFeature ADDSTools 
-        { 
-            Ensure = "Present" 
-            Name = "RSAT-ADDS-Tools"
-			DependsOn = "[WindowsFeature]ADDSInstall"
-        }  
 
-        xADDomain FirstDS 
+        WindowsFeature ADAdminCenter
+        {
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        WindowsFeature ADDSTools
+        {
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+        xADDomain FirstDS
         {
             DomainName = $DomainName
             DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
-            DatabasePath = "F:\NTDS"
-            LogPath = "F:\NTDS"
-            SysvolPath = "F:\SYSVOL"
-			DependsOn = "[WindowsFeature]ADDSInstall"
-        } 
+            DatabasePath = "D:\NTDS"
+            LogPath = "D:\NTDS"
+            SysvolPath = "D:\SYSVOL"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
 
-        LocalConfigurationManager 
+        LocalConfigurationManager
         {
             ConfigurationMode = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
-   }
-} 
+    }
+}
